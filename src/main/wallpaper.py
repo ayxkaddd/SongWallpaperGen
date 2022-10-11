83126e37491263a8d4e77d998ebc6aedcc609aa6
мама я программист <82116858+ayxkaddd@users.noreply.github.com>
2022-10-12 02:13:50 +0300

    actually... - :nerd:
    WTF DID I DO MAN

--- src/main/wallpaper.py
+++ src/main/wallpaper.py
@@ -5,8 +5,8 @@ import config
 class SetWallpaper:
 
     def __init__(self):
-        self.setwalppaper()
+        self.setwallpaper()
 
-    def setwalppaper(self):
+    def setwallpaper(self):
         full_path = f"{config.full_path}/spotify_wallpaper/src/resources/temp/tempBG.bmp"
         ctypes.windll.user32.SystemParametersInfoW(20, 0, full_path, 0)
