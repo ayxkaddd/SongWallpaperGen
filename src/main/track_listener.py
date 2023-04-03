d155d6ce066778e3c2d16a049e0ed5fa172f69a9
ayxdafish <82116858+ayxkaddd@users.noreply.github.com>
2023-04-03 15:03:48 +0300

    Update track_listener.py

--- src/main/track_listener.py
+++ src/main/track_listener.py
@@ -1,39 +1,45 @@
-import os
 import sys
+sys.path.insert(0, '...')
+
+import os
 import wget
 import requests
-
 from SwSpotify import spotify
 from SwSpotify import SpotifyNotRunning, SpotifyPaused
 
-import src.main.config as config
+import config
+
 
 class CurrentTrack:
 
-    def __init__(self):
-        pass
-
-
     def return_track(self):
         try:
+            # this fucking shit breaking whole app 💀
             string = f"{spotify.artist()}%20-%20{spotify.song()}"
             if "#" in string:
                 string = string.replace("#", "")
             return string
-        except SpotifyNotRunning as not_running:
-            print(f"[{not_running}]")
-        except SpotifyPaused as paused:
-            print(f"[{paused}]")
+        except SpotifyNotRunning:
+            raise Exception("Spotify is not running")
+        except SpotifyPaused:
+            raise Exception("Spotify is paused") 
+        except ImportError as imperr:
+            raise Exception("ImportError:", imperr)
+        except:
+            return "Unknown"
 
 
     def pretty_string(self):
         try:
-            prettystring = f"{spotify.artist()} - {spotify.song()}"
-            return prettystring
-        except SpotifyNotRunning as not_running:
-            print("spotify is not running")
-        except SpotifyPaused as paused:
-            print("spotify paused brbrbr")
+            return f"{spotify.artist()} - {spotify.song()}"
+        except SpotifyNotRunning:
+            raise Exception("Spotify is not running")
+        except SpotifyPaused:
+            raise Exception("Spotify is paused") 
+        except ImportError as imperr:
+            raise Exception("ImportError:", imperr)
+        except:
+            return "Unknown"
 
 
 class GetCover:
@@ -47,49 +53,46 @@ class GetCover:
 
 
     def download(self, url):
-        full_path = f"{config.full_path}/spotify_wallpaper/src/resources/temp_layers"
-        dirlist = os.listdir(full_path)
+        full_path = fr"{config.full_path}\src\resources\temp_layers"
         try:
-            ls = dirlist.pop()
-            if "temp_layer.png" in ls:
-                os.remove(f"{full_path}/temp_layer.png")
-                print("[1] downloading")
-                wget.download(url, f"{full_path}/temp_layer.png")
-            else:
-                print("[0] downloading")
-                wget.download(url, f"{full_path}/temp_layer.png")
+            os.makedirs(full_path, exist_ok=True)
+            temp_layer_path = os.path.join(full_path, "temp_layer.png")
+            if os.path.exists(temp_layer_path):
+                os.remove(temp_layer_path)
+            wget.download(url, temp_layer_path)
         except Exception as e:
-            print("[2] downloading")
-            wget.download(url, f"{full_path}/temp_layer.png")
+            print("Error downloading cover:", e)
 
 
     def get_cover(self):
-        search_line = CurrentTrack()
-        search_line = search_line.return_track()
-        if search_line == None:
+        try:
+            search_line = CurrentTrack().return_track()
+            if search_line == "Unknown":
+                self.waiting()
+            else:
+                AUTH_URL = "https://accounts.spotify.com/api/token"
+                auth_response = requests.post(AUTH_URL, {
+                    "grant_type": "client_credentials",
+                    "client_id": f"{config.client_id}",
+                    "client_secret": f"{config.client_secret}",
+                })
+
+                auth_response_data = auth_response.json()
+                access_token = auth_response_data["access_token"]
+                headers = {
+                    "Authorization": f"Bearer {access_token}"
+                }
+
+                base_url = "https://api.spotify.com/v1"
+                bang = f"{base_url}/search?q={search_line}&type=track&offset=0&limit=1"
+                response = requests.get(bang, headers=headers)
+                resp = response.json()
+                items = resp.get("tracks").get("items")[0]
+                coverURL = items.get("album").get("images")[1].get("url")
+                self.download(coverURL)
+        except SpotifyNotRunning:
             self.waiting()
-        else:
-            AUTH_URL = "https://accounts.spotify.com/api/token"
-            auth_response = requests.post(AUTH_URL, {
-                'grant_type': 'client_credentials',
-                'client_id': f"{config.client_id}",
-                'client_secret': f"{config.client_secret}",
-            })
-
-            auth_response_data = auth_response.json()
-            access_token = auth_response_data['access_token']
-            headers = {
-            'Authorization': 'Bearer {token}'.format(token=access_token)
-            }
-
-            base_url = 'https://api.spotify.com/v1'
-            response = requests.get(f"{base_url}/search?q={search_line}&type=track&offset=0&limit=1", headers=headers)
-            resp = response.json()
-            tracks = resp.get("tracks")
-            items = tracks.get("items")
-            items = items[0]
-            album = items.get("album")
-            images = album.get("images")
-            images = images[1]
-            coverURL = images.get("url")
-            self.download(coverURL)
+        except SpotifyPaused:
+            print("Playback is paused")
+        except Exception as e:
+            print("Failed to get cover image:", e)
