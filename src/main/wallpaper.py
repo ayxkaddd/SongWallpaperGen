13fcd5126b14b9b540d02ba13cd79dd433ec4e34
ayxdafish <82116858+ayxkaddd@users.noreply.github.com>
2023-04-03 15:04:13 +0300

    Update wallpaper.py

--- src/main/wallpaper.py
+++ src/main/wallpaper.py
@@ -1,15 +1,15 @@
-import ctypes
+import sys
+sys.path.insert(0, '...')
 
-import src.main.config as config
+import ctypes
+import config
 
 class SetWallpaper:
 
-    def __init__(self):
-        pass
-
     def setwallpaper(self):
-        full_path = f"{config.full_path}/spotify_wallpaper/src/resources/temp/tempBG.bmp"
+        full_path = fr"{config.full_path}\src\resources\temp\tempBG.bmp"
         ctypes.windll.user32.SystemParametersInfoW(20, 0, full_path, 0)
+
 
     def set_back(self):
         ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{config.wallpaper_path}", 0)
