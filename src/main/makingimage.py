c82e011a365f2e7d4c690426f9e5912592a7c71e
ayxdafish <82116858+ayxkaddd@users.noreply.github.com>
2023-04-03 15:03:12 +0300

    Update makingimage.py

--- src/main/makingimage.py
+++ src/main/makingimage.py
@@ -1,36 +1,38 @@
-import os
+import sys
+sys.path.insert(0, '...')
+import config
+
 from PIL import Image, ImageDraw, ImageFont
 
-import src.main.config as config
 
 class MakeImage:
 
-    def __init__(self, song, bg_path="default"):
+    def __init__(self, song, bg_path=fr"{config.full_path}\src\resources\background_test.png"):
         self.song = song
-        self.bg_path = f"{config.full_path}/spotify_wallpaper/src/resources/background_test.png"
+        self.bg_path = bg_path
         self.makeimage()
 
 
-    def makeimage(self, layer_path=f"{config.full_path}/spotify_wallpaper/src/resources/temp_layers/temp_layer.png"):
+    def makeimage(self):
         try:
-            bg = Image.open(self.bg_path)
-            layer = Image.open(layer_path)
-            area = (1650, 110)
-            PINK = (255, 0, 255)
-            fnt = ImageFont.truetype("comicbd.ttf", 50)
-            x = 950
+            layer_path=fr"{config.full_path}\src\resources\temp_layers\temp_layer.png"
+            full_bg_path = fr"{config.full_path}\src\resources\temp\tempBG.bmp"
+            bg = Image.open(fr"{self.bg_path}")
+            layer = Image.open(fr"{layer_path}")
+            x = 980
             y = 1250
+            AREA = (1650, 110)
+            PINK = (255, 0, 255)
+            FNT = ImageFont.truetype("comicbd.ttf", 50)
 
-            bg.paste(layer, area)
+            bg.paste(layer, AREA)
             first_text = ImageDraw.Draw(bg)
-            first_text.text((x, y-100), "HIS ASS IS LISTENING", font=fnt, fill=PINK)
+            first_text.text((x, y-100), "HIS ASS IS LISTENING", font=FNT, fill=PINK)
             track_text = ImageDraw.Draw(bg)
-            song_len = len(self.song) * 10
-
-            widht = 2560 / 2
-            x = (widht-song_len) - song_len / 2
-            track_text.text((x, y), f"{self.song}", font=fnt, fill=PINK)
-            full_bg_path = f"{config.full_path}/spotify_wallpaper/src/resources/temp/tempBG.bmp"
-            bg.save(f"{full_bg_path}")
+            song_len = FNT.getsize(self.song)[0] + len(self.song) * 5
+            widht = 2630
+            x = (widht - song_len) / 2
+            track_text.text((x, y), f"{self.song}", font=FNT, fill=PINK)
+            bg.save(fr"{full_bg_path}")
         except Exception as e:
             print(e)
