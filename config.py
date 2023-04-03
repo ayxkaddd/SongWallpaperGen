4153c2aa14be1a8bc7880cccf550968d729d7e5c
ayxdafish <82116858+ayxkaddd@users.noreply.github.com>
2023-04-03 15:02:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os 
+client_id = ""
+client_secret = ""
+full_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+set_back = False # setting back your wallpaper after closing the program
+wallpaper_path = "" # change if set_back = True
